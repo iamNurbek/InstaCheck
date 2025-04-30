ecdf40df4428a8d211da69c0c50faeaea5ae5fa9
iamNurbek <21njouraboev@gmail.com>
2025-04-29 22:07:53 -0400

    Fix: set allowed CORS origins for custom domain

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 import re
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["https://instacheck.us", "https://www.instacheck.us"])
 
 def extract_usernames(raw_text):
     lines = raw_text.splitlines()
